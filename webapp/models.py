73d903946f1ae7a9b092fb43b50b86d386dde32a
FluidSense <johanneskv96@gmail.com>
2017-09-19 16:50:19 +0200

    Created all model fields, methods remain.

--- webapp/models.py
+++ webapp/models.py
@@ -9,3 +9,18 @@ class Konserter(models.Model):
     dato = models.DateTimeField(blank=True, null=True)
     band = models.CharField(max_length=200)
     festival = models.CharField(max_length=200)
+
+class Band(models.Model):
+    navn = models.CharField(max_length=200)
+    kostnad = models.IntegerField()
+    manager = models.ForeignKey('auth.User')
+    utstyr = models.TextField()
+    sjanger = models.CharField(max_length=100)
+    info = models.TextField()
+    rating = models.IntegerField()
+
+class Bestilling(models.Model):
+    dato = models.DateTimeField(blank=True,null=True)
+    band = models.ForeignKey('models.Band')
+    scene = models.CharField(max_length=200)
+    godkjent = models.CharField(max_length=3)
