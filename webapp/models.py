87b3dbb2bb25acc2e27949d4eaea2f506036c27d
FluidSense <johanneskv96@gmail.com>
2017-09-19 11:55:31 +0200

    added konserter-model

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Konserter(models.Model):
+    scene = models.CharField(max_length=200)
+    teknikere= models.TextField()
+    konsert = models.CharField(max_length=200)
+    dato = models.DateTimeField(blank=True, null=True)
+    band = models.CharField(max_length=200)
+    festival = models.CharField(max_length=200)
