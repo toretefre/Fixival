63c18a95af147bc1fa6315fb220cec3a25a8559f
FluidSense <johanneskv96@gmail.com>
2017-09-20 17:05:37 +0200

    Added some URLs, started on login and made /arrangoer to show concerts.

--- it1901/urls.py
+++ it1901/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from webapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts',include('django.contrib.auth.urls')),
+    url(r'^arrangoer',views.arrangoer_mainpage,name='webapp/arrangoer_mainpage')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Konserter
 
 # Create your views here.
+def arrangoer_mainpage(request):
+    konserts = Konserter.objects.all()
+    return render(request,'webapp/arrangoer_mainpage.html',{'konserts':konserts})
