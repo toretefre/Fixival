989a967dab23126e53bd7fcc3c729e9d361dd10c
trymhog97 <tehogeli@stud.ntnu.no>
2017-09-21 10:43:17 +0200

    Started on technician view

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Konserter
 # Create your views here.
+def tech_view(request):
+    konserter = Konserter.objects.all()
+    return render(request, "webapp/tekniker_view.html", {'konserts':konserter})
