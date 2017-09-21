0981d05b18c4b1b0e9cf33a9c205b4f91f7b2233
trymhog97 <tehogeli@stud.ntnu.no>
2017-09-21 11:28:20 +0200

    added import in views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Konserter
+from django.contrib.auth.models import User
 # Create your views here.
 def tech_view(request):
     konserter = Konserter.objects.all()
