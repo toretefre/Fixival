a51bcc91b3f7b6b3adad2b921315d499b183d38c
trymhog97 <tehogeli@stud.ntnu.no>
2017-09-21 11:18:30 +0200

    changes to tekniker_view.html and queryset in views.py

--- webapp/views.py
+++ webapp/views.py
@@ -3,4 +3,5 @@ from .models import Konserter
 # Create your views here.
 def tech_view(request):
     konserter = Konserter.objects.all()
-    return render(request, "webapp/tekniker_view.html", {'konserts':konserter})
+    users = User.objects.all()
+    return render(request, "webapp/tekniker_view.html", {'konserts':konserter}, {'brukere':users})
