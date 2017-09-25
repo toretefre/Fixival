28b2829cf0c9c70b7db78541339e64d37b2e8b80
FluidSense <johanneskv96@gmail.com>
2017-09-25 10:31:45 +0200

    Finished dropdown for concerts.

--- webapp/static/js/dropdown.js
+++ webapp/static/js/dropdown.js
@@ -1,14 +1,23 @@
+// Tested on JSFiddle
+
 konsertheaders = document.getElementsByClassName('konsertheader');
 for(var i = 0; i < konsertheaders.length;i++){
   konsertheaders[i].addEventListener("click",function(){
     if(this.nextSibling.innerHTML == undefined){
-      if(this.nextSibling.nextSibling.style.display == "block"){
+      if(this.nextSibling.nextSibling.style.display == "inline"){
         this.nextSibling.nextSibling.style.display = "none";
+      }
+      else if(this.nextSibling.nextSibling.style.display == "none"){
+        this.nextSibling.nextSibling.style.display = "inline";
       }
     }
     else{
-      if(this.nextSibling.style.display == "block"){
+      if(this.nextSibling.style.display == "inline"){
         this.nextSibling.style.display = "none";
     }
+    else if(this.nextSibling.style.display == "none"){
+      this.nextSibling.style.display = "inline";
+    }
+  }
   })
 }
