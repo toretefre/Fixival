d86248584f4eaad0360b24d088f26f2dbad0deef
FluidSense <johanneskv96@gmail.com>
2017-09-26 13:19:26 +0200

    Restricted access to pages according to groups.

--- webapp/views.py
+++ webapp/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from .models import Konserter
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def arrangoer_mainpage(request):
-    konserts = Konserter.objects.all()
-    return render(request,'webapp/arrangoer_mainpage.html',{'konserts':konserts})
+    if request.user.groups.filter(name="arrangoer").exists():
+        konserts = Konserter.objects.all()
+        return render(request,'webapp/arrangoer_mainpage.html',{'konserts':konserts})
+    else:
+        raise PermissionDenied
 
+@login_required
 def oversiktsview_konserter(request):
-    konserter = Konserter.objects.all()
-    scener = []
-    for konsert in konserter:
-        if konsert.scene not in scener:
-            scener.append(konsert.scene)
+    if request.user.groups.filter(name="arrangoer").exists():
+        konserter = Konserter.objects.all()
+        scener = []
+        for konsert in konserter:
+            if konsert.scene not in scener:
+                scener.append(konsert.scene)
 
-    return render(request, 'webapp/oversiktsview_konserter.html', {'konserter':konserter, 'scener':scener})
+        return render(request, 'webapp/oversiktsview_konserter.html', {'konserter':konserter, 'scener':scener})
+    else:
+        raise PermissionDenied
