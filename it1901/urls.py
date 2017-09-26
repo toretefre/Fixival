2225890a302513972e688c2991ef0fd4a389bd5f
Tore Stensaker Tefre <tore@Tores-MacBook-Air.local>
2017-09-26 17:06:06 +0200

    more loginstuff, missing redirects

--- it1901/urls.py
+++ it1901/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
+    # index
+    url(r'^$', views.login, name='login'),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def login():
+    return HttpResponse("login")
+
+
+@login_required
+def logout(request):
+    return HttpResponse("User logged out")
