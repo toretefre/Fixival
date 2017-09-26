fd5d63bc90f6553d527d657fdebcd3c1207d0b77
FluidSense <johanneskv96@gmail.com>
2017-09-26 11:50:49 +0200

    Added group access to dynamic menu. Changed URL-names.

--- webapp/views.py
+++ webapp/views.py
@@ -5,3 +5,12 @@ from .models import Konserter
 def arrangoer_mainpage(request):
     konserts = Konserter.objects.all()
     return render(request,'webapp/arrangoer_mainpage.html',{'konserts':konserts})
+
+def oversiktsview_konserter(request):
+    konserter = Konserter.objects.all()
+    scener = []
+    for konsert in konserter:
+        if konsert.scene not in scener:
+            scener.append(konsert.scene)
+
+    return render(request, 'webapp/oversiktsview_konserter.html', {'konserter':konserter, 'scener':scener})
