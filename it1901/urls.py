10160488365a765e28061317a0c767e702a7fcad
trymhog97 <tehogeli@stud.ntnu.no>
2017-09-28 08:17:20 +0200

    trying to fix templatenotexist bug

--- it1901/urls.py
+++ it1901/urls.py
@@ -20,5 +20,5 @@ from webapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^tekniker', views.tech_view, name = "webapp/tech_view"),
+    url(r'^tekniker', views.tech_view, name = 'webapp/tech_view'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 def tech_view(request):
     konserter = Konserter.objects.all()
     users = User.objects.all()
-    return render(request, "webapp/tekniker_view.html", {'konserts':konserter}, {'brukere':users})
+    return render(request, "webapp/tekniker_view.html", {'konserts': konserter}, {'brukere': users})
