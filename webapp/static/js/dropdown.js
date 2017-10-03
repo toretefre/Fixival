d7d3f5793789c94181ab719729aac170260f6810
wowolala <mariaiva@stud.ntnu.no>
2017-10-03 09:56:52 +0200

    Fikset dropdown menu funksjonen som kan gjenbrukes for alle class header

--- webapp/static/js/dropdown.js
+++ webapp/static/js/dropdown.js
@@ -1,23 +1,33 @@
 // Tested on JSFiddle
 
-konsertheaders = document.getElementsByClassName('konsertheader');
-for(var i = 0; i < konsertheaders.length;i++){
-  konsertheaders[i].addEventListener("click",function(){
-    if(this.nextSibling.innerHTML == undefined){
-      if(window.getComputedStyle(this.nextSibling.nextSibling, null).getPropertyValue('display') == "block"){
-        this.nextSibling.nextSibling.style.display = "none";
+dropdown_on_headers('bandheader');
+
+dropdown_on_headers('konsertheader');
+
+// Funker bare på class, går gjennom alle elementene i classen og viser det
+
+function dropdown_on_headers(header) {
+
+  header = document.getElementsByClassName(header);
+
+  for(var i = 0; i < header.length;i++){
+    header[i].addEventListener("click",function(){
+      if(this.nextSibling.innerHTML == undefined){
+        if(window.getComputedStyle(this.nextSibling.nextSibling, null).getPropertyValue('display') == "block"){
+          this.nextSibling.nextSibling.style.display = "none";
+        }
+        else if(window.getComputedStyle(this.nextSibling.nextSibling, null).getPropertyValue('display') == "none"){
+          this.nextSibling.nextSibling.style.display = "block";
+        }
       }
-      else if(window.getComputedStyle(this.nextSibling.nextSibling, null).getPropertyValue('display') == "none"){
-        this.nextSibling.nextSibling.style.display = "block";
+      else{
+        if(window.getComputedStyle(this.nextSibling, null).getPropertyValue('display') == "block"){
+          this.nextSibling.style.display = "none";
+        }
+        else if(window.getComputedStyle(this.nextSibling, null).getPropertyValue('display') == "none"){
+          this.nextSibling.style.display = "block";
+        }
       }
-    }
-    else{
-      if(window.getComputedStyle(this.nextSibling, null).getPropertyValue('display') == "block"){
-        this.nextSibling.style.display = "none";
-    }
-    else if(window.getComputedStyle(this.nextSibling, null).getPropertyValue('display') == "none"){
-      this.nextSibling.style.display = "block";
-    }
+    })
   }
-  })
-}
+};
